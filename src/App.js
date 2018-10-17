9d8f5a311ae0fe12dfb3944c2b825bc8c31e3bb0
TechiePriyaRanjan <hackerswanted2012@gmail.com>
2018-10-18 01:14:48 +0530

    Using Class Component

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ const Pet = (props) => {
     ]);
 }
 
-const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet, {
-            name : "Luna",
-            animal : "Dog",
-            breed : "havanese"
-        }),
-        React.createElement(Pet, {
-            name : "Kadali",
-            animal : "Tiger",
-            breed : "Cheetah"
-        }),
-        React.createElement(Pet, {
-            name : "Meow",
-            animal : "Cat",
-            breed : "MeowMeow"
-        })
-    ])
+
+class App extends React.Component {
+    handleTitleClick(){
+        alert("You've clicked the title");
+    }
+    render() {
+        return React.createElement('div', {}, [
+            React.createElement('h1', { onClick : this.handleTitleClick }, 'Adopt Me!'),
+            React.createElement(Pet, {
+                name : "Luna",
+                animal : "Dog",
+                breed : "havanese"
+            }),
+            React.createElement(Pet, {
+                name : "Kadali",
+                animal : "Tiger",
+                breed : "Cheetah"
+            }),
+            React.createElement(Pet, {
+                name : "Meow",
+                animal : "Cat",
+                breed : "MeowMeow"
+            })
+        ])
+    }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'))
